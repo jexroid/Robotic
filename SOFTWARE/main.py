a3d7bac328fca5421d0cda6b7f79b3d13c8b4344
amirreza farzan <amirfr2290@gmail.com>
2023-02-17 14:19:21 +0330

    color.py has changed

--- SOFTWARE/main.py
+++ SOFTWARE/main.py
@@ -4,15 +4,7 @@ from modules import color
 from modules import serial_communication as sc
 
 
-def main():
-    cap = cv.VideoCapture(0)
-    while True:
-        _, frame = cap.read()
-
-        # detecting the color of the object
-        color_of_object = color.detect_color(frame)
-        return color_of_object
-
+color_of_object = color.color()
 
 
 
@@ -28,12 +20,11 @@ while True:
     if response == "color":
         print(response)
         # run the color detection
-        color_of_object = main()
 
         print(color_of_object, "is the color of object")
         sc.communicate.send(color_of_object)
 
         print(color_of_object, "has sended to HARDWARE")
         break
-
+        
     print("the job is done")
--- SOFTWARE/modules/color.py
+++ SOFTWARE/modules/color.py
@@ -1,93 +1,81 @@
-def color(INPUT):
-
+def detect():
+    from colorama import Fore
     import cv2 as cv
     import numpy as np
 
-    cap = cv.VideoCapture(1)
-    kernal = np.ones((5, 5), "uint8")
+    cap = cv.VideoCapture(0)
 
     yellow_range = [np.array([0, 167, 117]), np.array([30, 227, 241])]
     red_range = [np.array([0, 145, 170]),    np.array([10, 255, 255])]
     blue_range = [np.array([110, 155, 95]), np.array([125, 255, 255])]
     green_range = [(np.array([45, 100, 55])), (np.array([105, 255, 255]))]
-
-    # For red color
-
-    if INPUT == 'y':
-        INPUT = yellow_range
-    elif INPUT == 'r':
-        INPUT = red_range
-    elif INPUT == 'b':
-        INPUT = blue_range
-    elif INPUT == 'g':
-        INPUT = green_range
-
-    lower = INPUT[0]
-    upper = INPUT[1]
-    kernel = np.ones((6, 6), np.uint8)
-
+    kernal = np.ones((5, 5), "uint8")
     # making sure and reducing the errors
-    CERTAINITY = 0
-    timer = 0
 
-    while (timer < 50):
+    while (True):
         ### defining video captureing ###
         rec, frame = cap.read()
+
+        frame = cv.rectangle(frame, (324, 241), (321, 245), (255, 0, 0), 4)
 
         hsv = cv.cvtColor(
             frame,
             cv.COLOR_BGR2HSV
         )
 
-        mask_yellow = cv.inRange(
-            hsv,
-            lower,
-            upper
-        )
+        mask_yellow = cv.inRange(hsv, yellow_range[0], yellow_range[1])
+        mask_green = cv.inRange(hsv, green_range[0], green_range[1])
+        mask_blue = cv.inRange(hsv, blue_range[0], blue_range[1])
+        mask_red = cv.inRange(hsv, red_range[0], red_range[1])
+        all_masks = {
+            "yellow": mask_yellow,
+            "green": mask_green,
+            "blue": mask_blue,
+            "red": mask_red
+        }
 
-        color_mask = cv.dilate(mask_yellow, kernel)
-        color_mask = cv.morphologyEx(color_mask, cv.MORPH_CLOSE, kernel)
-        color_mask = cv.morphologyEx(color_mask, cv.MORPH_OPEN, kernel)
+        for text, mask in all_masks.items():
+            color_mask = cv.dilate(mask, kernel=kernal)
+            color_mask = cv.morphologyEx(
+                color_mask, cv.MORPH_CLOSE, kernel=kernal)
+            color_mask = cv.morphologyEx(
+                color_mask, cv.MORPH_OPEN, kernel=kernal)
 
-        ### finding the contours of image ###
-        contours, salam = cv.findContours(
-            color_mask,
-            cv.RETR_TREE,
-            cv.CHAIN_APPROX_NONE
-        )
+            ### finding the contours of image ###
+            contours, salam = cv.findContours(
+                color_mask,
+                cv.RETR_TREE,
+                cv.CHAIN_APPROX_NONE
+            )
 
-        ### NOW LETS DRAW THE FOUND CONTOURS with condition ###
-        for contour in contours:
-            cv.drawContours(frame, contour, -1, (0, 255, 0), 3)
-            area = cv.contourArea(contour)
+            ### NOW LETS DRAW THE FOUND CONTOURS with condition ###
+            for contour in contours:
 
-            if int(area) > 1000:
-                x, y, w, h = cv.boundingRect(contour)
+                area = cv.contourArea(contour)
 
-                frame = cv.rectangle(
-                    frame, (x, y), (x + w, y + h), (255, 0, 255), 2)
+                color_pixels = cv.countNonZero(mask)
+                print(Fore.GREEN, f"{text} pixels :", Fore.WHITE, color_pixels)
 
-        # cv.imshow('mask green', color_mask)
-        # cv.imshow('resault', frame)
+                if int(area) > 1000:
+                    x, y, w, h = cv.boundingRect(contour)
 
-        color_pixels = cv.countNonZero(mask_yellow)
-        print("pixels :", color_pixels)
+                    frame = cv.rectangle(
+                        frame, (x, y), (x + w, y + h), (255, 255, 20), 2)
+                    frame = cv.putText(
+                        frame, text, (x, y), cv.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 20))
 
-        if color_pixels > 10000:
-            CERTAINITY = CERTAINITY + 1
+                    
+                    
 
-        timer = timer + 1
+        
 
-        if CERTAINITY >= 30:
-            return True
+        cv.imshow('mask green', color_mask)
+        cv.imshow('resault', frame)
 
-        # if cv.waitKey(10) & 0xFF == ord('q'):
-        #     cap.release()
-        #     cv.destroyAllWindows()
-        #     break
-
-    if CERTAINITY <= 30:
-        return False
+        if cv.waitKey(10) & 0xFF == ord('q'):
+            cap.release()
+            cv.destroyAllWindows()
+            break
 
 
-print(color('b'))
+print(detect())
